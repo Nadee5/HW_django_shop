d3755be3d1db831034325a893167060f6a164796
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-01-10 23:03:09 +0300

    changes in slug (to auto recording - unique=True, blank=True), is_published (Binary -> Boolean)

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,11 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Blog(models.Model):
     title = models.CharField(max_length=250, verbose_name='Заголовок')
-    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    slug = models.CharField(max_length=150, verbose_name='slug', unique=True, blank=True)
     content = models.TextField(verbose_name='Содержимое')
     image = models.ImageField(upload_to='blog/', verbose_name='Изображение', **NULLABLE)
     date_of_create = models.DateField(auto_now=True, verbose_name='Дата создания')
-    is_published = models.BinaryField(default=True, verbose_name='ошибочка вышла')
-    is_activ = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     views_count = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
