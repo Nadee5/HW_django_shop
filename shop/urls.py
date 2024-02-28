c1f6ed28e755b6cf77953e45ea2a4d0fb303fc9c
Nadezhda Lyovushkina <lyovushkina.nv@gmail.com>
2024-02-28 17:42:56 +0300

    cached ProductDetailView

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from shop.apps import ShopConfig
 from shop.views import home, contacts, CategoryListView, ProductListView, ProductDetailView, ProductCreateView, \
@@ -13,7 +14,7 @@ urlpatterns = [
     path('contacts/', contacts, name="contacts"),
     path('catalog/', CategoryListView.as_view(), name="catalog"),
     path('products_category/<int:pk>', ProductListView.as_view(), name="products_category"),
-    path('product/<int:pk>', ProductDetailView.as_view(), name="product"),
+    path('product/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name="product"),
     path('all_products/', ProductAllListView.as_view(), name="all_products_list"),
     path('product/create/', ProductCreateView.as_view(), name="product_create"),
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name="product_update"),
